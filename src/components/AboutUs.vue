be963d11c8266a710689670af09dd6850988b75e
bijoymalaker <bijoymalaker7@gmail.com>
2025-02-10 00:11:46 +0600

    minor bug fixed

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -16,60 +16,6 @@
                         </ul>
                     </div>
                 </div>
-
-                <!-- Service cards -->
-                <!-- <div class="row text-center mt-4">
-
-                    <div class="col-md-4">
-                        <ul class="list-group">
-                            <li class="list-group-item active" aria-current="true">An active item</li>
-                            <li class="list-group-item">A second item</li>
-                            <li class="list-group-item">A third item</li>
-                            <li class="list-group-item">A fourth item</li>
-                            <li class="list-group-item">And a fifth one</li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-8">
-                        <div class="row">
-                            <div class="col-md-4 service-card">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="service-icon">🔍</div>
-                                        <h5 class="card-title">How does Order.UK work?</h5>
-                                        <p class="card-text">Description of how the service works.</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-4 service-card">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="service-icon">🛒</div>
-                                        <h5 class="card-title">Place an Order!</h5>
-                                        <p class="card-text">Description of how to place an order.</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-4 service-card">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="service-icon">📈</div>
-                                        <h5 class="card-title">Track Progress</h5>
-                                        <p class="card-text">Description of how to track progress.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <p class="text-center">Order.UK simplifies the food ordering process. Browse through our diverse
-                            menu, select your favorite dishes, and proceed to checkout. Your delicious meal will be on
-                            its
-                            way to your doorstep in no time!</p>
-                    </div>
-                </div> -->
-
-
                 <div class="row align-items-center about-nav">
                     <div class="col-4">
                         <!-- Tab navs -->
